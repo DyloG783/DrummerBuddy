5b4d58791493ec3aa93d5f6f2e193e0f1e0068b1
dylandigby87 <dylan.m.c.digby@gmail.com>
2024-05-01 21:45:36 +1000

    added role based authorization

--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -28,7 +28,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>() // Add this line to enable user roles
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
@@ -59,5 +60,47 @@ app.MapRazorComponents<App>()
 
 // Add additional endpoints required by the Identity /Account Razor components.
 app.MapAdditionalIdentityEndpoints();
+
+// create roles
+using (var scope = app.Services.CreateScope()) 
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var roles = new[] { "Admin", "User" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
+
+// seed users
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    string email1 = "admin@admin.com";
+    string password1 = "Admin123!";
+
+    string email2 = "user@user.com";
+    string password2 = "User123!";
+
+    if (await userManager.FindByEmailAsync(email1) == null)
+    {
+        var user = new ApplicationUser { UserName = email1, Email = email1 };
+        await userManager.CreateAsync(user, password1);
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
+
+    if (await userManager.FindByEmailAsync(email2) == null)
+    {
+        var user = new ApplicationUser { UserName = email2, Email = email2 };
+        await userManager.CreateAsync(user, password2);
+        await userManager.AddToRoleAsync(user, "User");
+    }
+}
 
 app.Run();
